3a4d3be370a4295f059007417810c01f1e6768fa
Fedor <fedor@dps.uibk.ac.at>
2020-12-24 10:05:04 +0100

    moved the interface for the graph provider into model

---
+++ src/main/java/at/uibk/dps/ee/model/graph/EnactmentGraphProvider.java
@@ -0,0 +1,18 @@
+package at.uibk.dps.ee.model.graph;
+
+/**
+ * Interface for the classes used for the generation of the
+ * {@link EnactmentGraph}.
+ * 
+ * @author Fedor Smirnov
+ *
+ */
+public interface EnactmentGraphProvider {
+
+	/**
+	 * Returns the {@link EnactmentGraph} modeling the enactment process.
+	 * 
+	 * @return the {@link EnactmentGraph} modeling the enactment process
+	 */
+	EnactmentGraph getEnactmentGraph();
+}
