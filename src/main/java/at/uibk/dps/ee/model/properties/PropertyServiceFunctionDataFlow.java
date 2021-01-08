441ea69fa6a66ae3e29546b5230d1e6770c8535f
Fedor <fedor@dps.uibk.ac.at>
2021-01-08 13:57:05 +0100

    renaming

--- src/main/java/at/uibk/dps/ee/model/properties/PropertyServiceFunctionDataFlow.java
+++ src/main/java/at/uibk/dps/ee/model/properties/PropertyServiceFunctionDataFlow.java
@@ -5,7 +5,7 @@ import net.sf.opendse.model.Task;
 import net.sf.opendse.model.properties.AbstractPropertyService;
 
 /**
- * Static method container to access properties of syntax function nodes.
+ * Static method container to access properties of data flow function nodes.
  * 
  * @author Fedor Smirnov
  */
@@ -25,18 +25,18 @@ public final class PropertyServiceFunctionDataFlow extends AbstractPropertyServi
 	 */
 	protected enum Property {
 		/**
-		 * The type of the syntax function
+		 * The type of the data flow function
 		 */
 		DataFlowType
 	}
 
 	/**
-	 * Types of syntax functions.
+	 * Types of data flow functions.
 	 * 
 	 * @author Fedor Smirnov
 	 *
 	 */
-	public enum SyntaxType {
+	public enum DataFlowType {
 		/**
 		 * Forwards the earliest available input to all outputs.
 		 */
@@ -44,52 +44,52 @@ public final class PropertyServiceFunctionDataFlow extends AbstractPropertyServi
 	}
 
 	/**
-	 * Creates a syntax function with the given ID and the given function type
+	 * Creates a data flow function with the given ID and the given function type
 	 * 
-	 * @param taskId     the id of the task to create
-	 * @param syntaxType the syntax type
-	 * @return a syntax function with the given ID and the given function type
+	 * @param taskId       the id of the task to create
+	 * @param dataFlowType the data flow type
+	 * @return a data flow function with the given ID and the given function type
 	 */
-	public static Task createSyntaxFunction(final String taskId, final SyntaxType syntaxType) {
+	public static Task createDataFlowFunction(final String taskId, final DataFlowType dataFlowType) {
 		final Task result = new Task(taskId);
 		PropertyServiceFunction.setType(FunctionType.DataFlow, result);
-		PropertyServiceFunctionDataFlow.setSyntaxType(result, syntaxType);
+		PropertyServiceFunctionDataFlow.setDataFlowType(result, dataFlowType);
 		return result;
 	}
 
 	/**
-	 * Returns the syntax type for the given task.
+	 * Returns the data flow type for the given task.
 	 * 
 	 * @param task the given task
-	 * @return the syntax type for the given task
+	 * @return the data flow type for the given task
 	 */
-	public static SyntaxType getSyntaxType(final Task task) {
+	public static DataFlowType getDataFlowType(final Task task) {
 		checkTask(task);
 		final String attrName = Property.DataFlowType.name();
-		return SyntaxType.valueOf((String) getAttribute(task, attrName));
+		return DataFlowType.valueOf((String) getAttribute(task, attrName));
 	}
 
 	/**
-	 * Sets the syntax type for the given task.
+	 * Sets the data flow type for the given task.
 	 * 
 	 * @param task the given task
 	 * @param type the type to set
 	 */
-	public static void setSyntaxType(final Task task, final SyntaxType type) {
+	public static void setDataFlowType(final Task task, final DataFlowType type) {
 		checkTask(task);
 		final String attrName = Property.DataFlowType.name();
 		task.setAttribute(attrName, type.name());
 	}
 
 	/**
-	 * Checks that the given task is a syntax task throws an exception otherwise.
+	 * Checks that the given task is a data flow task throws an exception otherwise.
 	 * 
 	 * @param task the task to check
 	 */
 	protected static void checkTask(final Task task) {
 		PropertyServiceFunction.checkTask(task);
 		if (!PropertyServiceFunction.getType(task).equals(FunctionType.DataFlow)) {
-			throw new IllegalArgumentException("Task " + task.getId() + " is not a syntax task");
+			throw new IllegalArgumentException("Task " + task.getId() + " is not a data flow task");
 		}
 	}
 }
--- src/test/java/at/uibk/dps/ee/model/properties/PropertyServiceFunctionDataFlowTest.java
+++ src/test/java/at/uibk/dps/ee/model/properties/PropertyServiceFunctionDataFlowTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import at.uibk.dps.ee.model.properties.PropertyServiceFunction.FunctionType;
-import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow.SyntaxType;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunctionDataFlow.DataFlowType;
 import net.sf.opendse.model.Task;
 
 public class PropertyServiceFunctionDataFlowTest {
@@ -13,11 +13,11 @@ public class PropertyServiceFunctionDataFlowTest {
 	@Test
 	public void testCreateSyntaxFunction() {
 		String taskId = "id";
-		SyntaxType syntaxType = SyntaxType.EarliestInput;
-		Task result = PropertyServiceFunctionDataFlow.createSyntaxFunction(taskId, syntaxType);
+		DataFlowType syntaxType = DataFlowType.EarliestInput;
+		Task result = PropertyServiceFunctionDataFlow.createDataFlowFunction(taskId, syntaxType);
 		assertEquals(taskId, result.getId());
 		assertEquals(FunctionType.DataFlow, PropertyServiceFunction.getType(result));
-		assertEquals(SyntaxType.EarliestInput, PropertyServiceFunctionDataFlow.getSyntaxType(result));
+		assertEquals(DataFlowType.EarliestInput, PropertyServiceFunctionDataFlow.getDataFlowType(result));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -31,7 +31,7 @@ public class PropertyServiceFunctionDataFlowTest {
 	public void checkSetGetSyntaxType() {
 		Task task = new Task("task");
 		PropertyServiceFunction.setType(FunctionType.DataFlow, task);
-		PropertyServiceFunctionDataFlow.setSyntaxType(task, SyntaxType.EarliestInput);
-		assertEquals(SyntaxType.EarliestInput, PropertyServiceFunctionDataFlow.getSyntaxType(task));
+		PropertyServiceFunctionDataFlow.setDataFlowType(task, DataFlowType.EarliestInput);
+		assertEquals(DataFlowType.EarliestInput, PropertyServiceFunctionDataFlow.getDataFlowType(task));
 	}
 }
