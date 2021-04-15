b583f4590fd4b4ce0691ee9db6ada8f91e952135
Fedor <fedor@dps.uibk.ac.at>
2021-04-15 13:39:14 +0200

    minor adjustment for constructing enactment and resource graph

--- src/main/java/at/uibk/dps/ee/model/graph/EnactmentGraph.java
+++ src/main/java/at/uibk/dps/ee/model/graph/EnactmentGraph.java
@@ -1,5 +1,6 @@
 package at.uibk.dps.ee.model.graph;
 
+import edu.uci.ics.jung.graph.util.EdgeType;
 import net.sf.opendse.model.Application;
 import net.sf.opendse.model.Dependency;
 import net.sf.opendse.model.Task;
@@ -13,5 +14,25 @@ import net.sf.opendse.model.Task;
 public class EnactmentGraph extends Application<Task, Dependency> {
 
   private static final long serialVersionUID = 1L;
-
+  
+  /**
+   * Default constructor.
+   */
+  public EnactmentGraph() {
+  }
+  
+  /**
+   * Constructor to build an {@link EnactmentGraph} out of an application.
+   * 
+   * @param application the given application
+   */
+  public EnactmentGraph(Application<Task, Dependency> application) {
+    super();
+    application.getEdges().forEach(edge -> {
+      Task src = application.getSource(edge);
+      Task dst = application.getDest(edge);
+      this.addEdge(edge, src, dst, EdgeType.DIRECTED);
+    });
+    application.forEach(task -> this.addVertex(task));
+  }
 }
--- src/main/java/at/uibk/dps/ee/model/graph/ResourceGraph.java
+++ src/main/java/at/uibk/dps/ee/model/graph/ResourceGraph.java
@@ -1,5 +1,6 @@
 package at.uibk.dps.ee.model.graph;
 
+import edu.uci.ics.jung.graph.util.EdgeType;
 import net.sf.opendse.model.Architecture;
 import net.sf.opendse.model.Link;
 import net.sf.opendse.model.Resource;
@@ -14,4 +15,25 @@ import net.sf.opendse.model.Resource;
 public class ResourceGraph extends Architecture<Resource, Link> {
 
   private static final long serialVersionUID = 1L;
+  
+  /**
+   * Default constructor.
+   */
+  public ResourceGraph() {
+  }
+  
+  /**
+   * Constructor to build the {@link ResourceGraph} out of an architecture.
+   * 
+   * @param arch the given architecture
+   */
+  public ResourceGraph(Architecture<Resource, Link> arch) {
+    super();
+    arch.getEdges().forEach(link -> {
+      Resource res1 = arch.getEndpoints(link).getFirst();
+      Resource res2 = arch.getEndpoints(link).getSecond();
+      this.addEdge(link, res1, res2, EdgeType.UNDIRECTED);
+    });
+    arch.forEach(res -> this.addVertex(res));
+  }
 }
